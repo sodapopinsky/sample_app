c41b58dd494867b2d5993b1be9fd749952eee7f1
Nick Spitale <nspita1@gmail.com>
2014-03-15 09:32:51 -0500

    fa

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SampleApp::Application.routes.draw do
+
+  get "users/new"
+  root to: 'static_pages#home'
+  match '/signup', to: 'users#new'
+  
   get "static_pages/home"
 
   get "static_pages/help"
